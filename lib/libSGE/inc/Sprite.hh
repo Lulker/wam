1fece5f9bac1c10eed91447b683c3bbd2fd748ff
Lulker <lulker@users.noreply.github.com>
2014-11-16 00:54:49 +0100

    removed some vintage code

--- lib/libSGE/inc/Sprite.hh
+++ lib/libSGE/inc/Sprite.hh
@@ -12,14 +12,9 @@ class Sprite : public BCO {
 		* @param angle optional angle to rotate sprite
 		* @param flip optional parameter to flip sprite
 		**/
-		void draw(const Vector2<> &position = {0,0}, const double &angle = 0, eK_RendererFlip flip = 0){
+		void draw(const Vector2<int> &position = {0,0}, const double &angle = 0, eK_RendererFlip flip = 0){
 			destination.x = position.x;
 			destination.y = position.y;
-			SDL_RenderCopyEx(GEK::renderer, texture, 0, &destination, angle, 0, flip);
-		}
-		void draw(int x, int y, const double &angle = 0, eK_RendererFlip flip = 0){
-			destination.x = x;
-			destination.y = y;
 			SDL_RenderCopyEx(GEK::renderer, texture, 0, &destination, angle, 0, flip);
 		}
 };
