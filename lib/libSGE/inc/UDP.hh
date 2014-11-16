ba2f119d5a41296c5a66c601f77ed308c0613417
Lulker <lulker@users.noreply.github.com>
2014-11-16 07:31:05 +0100

    Readded UDP back

---
+++ lib/libSGE/inc/UDP.hh
@@ -0,0 +1,16 @@
+class UDP : public BCO {
+	private:
+		struct sockaddr_in address = {};
+		int ld;
+	public:
+		~UDP();
+		UDP(unsigned short port = 0);
+		void to(unsigned long ip, unsigned short port){
+			address.sin_addr.s_addr = ip;
+			address.sin_port = port;
+		}
+		void to(const char* ip, unsigned short port){to(inet_addr(ip),htons(port));}
+		void write(const char* buffer, int len) {sendto(ld, buffer, len, 0, reinterpret_cast<const sockaddr*>(&address), sizeof(sockaddr_in));}
+		int readfrom(char* buffer, int len) {return recvfrom(ld,buffer,len,0,reinterpret_cast<sockaddr*>(&address),0);}
+		int read(char* buffer, int len) {return recvfrom(ld,buffer,len,0,0,0);}
+};
---
+++ lib/libSGE/src/UDP.cc
@@ -0,0 +1,27 @@
+#include <SGE.hh>
+#ifdef _WIN32
+	#define ioctl ioctlsocket
+	#define close closesocket
+#else
+	#include <sys/socket.h>
+	#include <sys/ioctl.h>
+	#include <unistd.h>
+#endif
+
+SGE::UDP::UDP(unsigned short port) {
+	#ifdef _WIN32
+	WSADATA info;
+	debug_assert(WSAStartup(514, &info));
+	#endif
+	debug_assert((ld = socket(2,2,0)),==-1);
+	long unsigned opt = 1;
+	ioctl(ld, FIONBIO, &opt);
+	address.sin_family = 2;
+	address.sin_port = htons(port);
+	debug_assert(bind(ld, reinterpret_cast<const sockaddr*>(&address), sizeof(sockaddr_in)),==-1);
+}
+
+SGE::UDP::~UDP(){
+	close(ld);
+	debug_point();
+}
