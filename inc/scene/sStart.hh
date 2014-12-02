98d82c872cfaa354aea931ecd1f386961e6282e5
slipy12 <jmaeso@enti.cat>
2014-12-02 20:19:28 +0100

    Started Menu with simple image click to start
    executable version

--- inc/scene/sStart.hh
+++ inc/scene/sStart.hh
@@ -1,4 +1,18 @@
 class sStart : public Scene {
+	Object *bg;
+	TMX *map;
 	public:
-		void init(){GEK::scene = new sGame;}
+		void init(){
+			bg = BC(Object,BC(Sprite,Surface("menu/Background.jpg")),Vector2(0,0),2);
+			map = BC(TMX,"maps/Map1.tmx",4);
+		}
+		void loop(const double &deltatime){
+			switch(GEK::mouse.status){
+				case (Mouse::L):
+					GEK::scene = new sGame;
+				default:break;
+			}
+			bg->update(deltatime);
+			bg->draw(map);
+		};
 };
