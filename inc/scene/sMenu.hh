bfe90ddcd8636410e1dd38143c22a0f96cc5d6eb
slipy12 <jmaeso@enti.cat>
2014-12-10 11:19:52 +0100

    Improved menu, added buttons

--- inc/scene/sMenu.hh
+++ inc/scene/sMenu.hh
@@ -1,26 +1,31 @@
 class sMenu : public Scene {
-	UI *bg;
-	UI *start;
+	enum elements {
+		background,
+		start,
+		exit,
+		start_hover,
+		exit_hover
+	};
+	UI *menu[5];
 	public:
 		void init(){
-			bg = BC(UI,Surface("menu/Background.jpg"));
-			start = BC(UI,Surface("menu/Start.png"),bg,Vector2(0.5),Vector2(0.5),Vector2(0.1,0.1*GEK::screen.x/GEK::screen.y));
-			on[eK_KEYDOWN] = [&](const eK_Event & e){
-				switch(e.key.keysym.sym){
-					case '\033':
-							std::exit(0);
-						break;
-				};
-			};
+			menu[background] = BC(UI,Surface("menu/Background.jpg"));
+			menu[start] = BC(UI,Surface("menu/Start.png"),menu[0],Vector2(0.3,0.9),Vector2(0.5),Vector2(0.2,0.1*aspectratio));
+			menu[exit] = BC(UI,Surface("menu/Exit.png"),menu[0],Vector2(0.7,0.9),Vector2(0.5),Vector2(0.2,0.1*aspectratio));
+			menu[start_hover] = BC(UI,Surface("menu/Start_hover.png"),menu[0],Vector2(0.3,0.9),Vector2(0.5),Vector2(0.2,0.1*aspectratio));
+			menu[exit_hover] = BC(UI,Surface("menu/Exit_hover.png"),menu[0],Vector2(0.7,0.9),Vector2(0.5),Vector2(0.2,0.1*aspectratio));
 		}
 		void loop(const double &deltatime){
-			switch(GEK::mouse.status){
-				case (Mouse::L):
-					if(start->contains(GEK::mouse.position))
-						GEK::scene = new sGame;
-				default:break;
+			for(int i=0;i<3;++i)
+				menu[i]->draw();
+			if(menu[start]->contains(GEK::mouse.position)){
+				menu[start_hover]->draw();
+				if(GEK::mouse.status && Mouse::L)
+					GEK::scene = new sGame;
+			} else if(menu[exit]->contains(GEK::mouse.position)){
+				menu[exit_hover]->draw();
+				if(GEK::mouse.status && Mouse::L)
+					std::exit(0);
 			}
-			bg->draw();
-			start->draw();
-		};
+		}
 };
