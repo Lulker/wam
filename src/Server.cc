3972735bfa942e5f1d3ec7ed3c8433f7e9db823f
NWDD <nwdd@chome.ws>
2015-01-19 19:39:46 +0100

    Winshit

--- src/Server.cc
+++ src/Server.cc
@@ -59,24 +59,16 @@ const bool anticheat( const Private& player ){
 
 void handler(UDP *sock){
     int size;
-    sockaddr_in player_address;
-    for(char Buffer[512];;) while((size = sock->read(Buffer,512,&player_address))>0)
-        switch(size){
-            case sizeof(Private):{
-                Private player = reinterpret_cast<Private&>(Buffer);
-                if(!player.secret){
-                    mutex.lock();
-                    char tmp = assign_team();
-                    players.push_back(Player(vrand(),players.size(),tmp,player_address,spawn_position(tmp),spawn_rotation(tmp)));
-                    sock->write(static_cast<Private*>(&players.back()),sizeof(Private),&player_address);
-                    mutex.unlock();
-                }
-                else if (player.id<players.size() && anticheat(player)){
-                    players[player.id].shoot = player.shoot;
-                    players[player.id].target = player.target;
-                }
-                break;
-            }
+    for(Player player;;) while((size = sock->read(static_cast<Private*>(&player),sizeof(Private),&player.address))==sizeof(Private))
+        if(!player.secret){
+            mutex.lock();
+            char tmp = assign_team();
+            players.push_back(Player(vrand(),players.size(),tmp,player.address,spawn_position(tmp),spawn_rotation(tmp)));
+            sock->write(static_cast<Private*>(&players.back()),sizeof(Private),&players.back().address);
+            mutex.unlock();
+        } else if (player.id<players.size() && anticheat(player)){
+            players[player.id].shoot = player.shoot;
+            players[player.id].target = player.target;
         }
 }
 
