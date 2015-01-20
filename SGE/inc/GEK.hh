66551783f3e8c27d393704b22ec1a83c83729237
NWDD <nwdd@chome.ws>
2015-01-20 22:06:29 +0100

    Singleton GameEngineKernel

--- SGE/inc/GEK.hh
+++ SGE/inc/GEK.hh
@@ -9,21 +9,67 @@ struct Mouse {
 	Vector2 position;
 };
 
-#define aspectratio GEK::screen.x/GEK::screen.y
+#define aspectratio (GEK->screen.x/GEK->screen.y)
 
-struct GEK {
-	static void clean();
-	static const char *server;
-	static eK_Window *window;
-	static eK_Renderer *renderer;
+struct GameEngineKernel {
+	///Window instance
+	eK_Window *window;
+	///Renderer instance
+	eK_Renderer *renderer;
+	///Contains font
+	eK_TTF font;
+	///Server that we will try to connect
+	const char *server;
 	///Scene that will run
-	static Scene *scene;
+	Scene *scene;
 	///Contains screen size
-	static Vector2 screen;
+	Vector2 screen;
 	///Contains mouse position
-	static Mouse mouse;
+	Mouse mouse;
+	///Singleton
+	GameEngineKernel(const char* title, const char* ip = "127.0.0.1"):server(ip),scene(0),mouse({Mouse::NONE,Mouse::NONE,{0,0}}){
+		assert(!SDL_Init(eK_INIT_EVERYTHING));
+		eK_DisplayMode info;
+		assert(!SDL_GetCurrentDisplayMode(0,&info));
+		screen = Vector2(info.w,info.h);
+		window = assert(SDL_CreateWindow(title, eK_WINDOWPOS_CENTERED, eK_WINDOWPOS_CENTERED, info.w, info.h, eK_WINDOW_SHOWN|eK_WINDOW_BORDERLESS|eK_WINDOW_MAXIMIZED));
+		renderer = assert(SDL_CreateRenderer(window, -1, eK_RENDERER_ACCELERATED | eK_RENDERER_PRESENTVSYNC));
+		assert(!TTF_Init());
+		font = TTF_OpenFont("fonts/FiraSans-Light.ttf", 18);
+	}
+	int loop(Scene* initial_scene){
+		scene = initial_scene;
+		for(Scene *cs;(cs = scene);delete cs)
+			for(int x,y; cs==scene;){
+				eK_Event e;
+				SDL_RenderClear(renderer);
+				while(SDL_PollEvent(&e))
+					switch(e.type){
+						case eK_WINDOWEVENT:
+							SDL_GetWindowSize(window,&x,&y);
+							screen = Vector2(x,y);
+						break;
+						case eK_QUIT:return 0;
+						default:if(cs->on.count(e.type))
+							cs->on[e.type](e);
+					}
+				Mouse::Button previous = mouse.status;
+				mouse = {(Mouse::Button)(SDL_GetMouseState(&x,&y) & Mouse::ANY),(Mouse::Button)0,Vector2(x,y)};
+				mouse.updated = Mouse::Button(previous^mouse.status);
+				scene = (*cs)();
+				SDL_RenderPresent(renderer);
+			}
+		return 0;
+	}
+	~GameEngineKernel(){
+		if(scene) delete scene;
+		if(renderer) SDL_DestroyRenderer(renderer);
+		if(window) SDL_DestroyWindow(window);
+		if(TTF_WasInit()) TTF_Quit();
+		SDL_Quit();
+	}
 	///Sets background color
-	static void bg(const int &red, const int &green, const int &blue){SDL_SetRenderDrawColor(renderer, red, green, blue, 255);};
-	///Scene manager
-	static int main(const char*, Scene*);
+	void bg(const int &red, const int &green, const int &blue){SDL_SetRenderDrawColor(renderer, red, green, blue, 255);};
 };
+
+extern GameEngineKernel *GEK;
--- inc/scene/sGame.hh
+++ inc/scene/sGame.hh
@@ -2,58 +2,60 @@
 #include <Player.hh>
 #include <UI.hh>
 
-class sGame : public Scene {
-	private:
-		TMX *map;
-		int id;
-		Sprite *green;
-		Sprite *red;
-		Surface *hp_fill;
-		UDP socket;
-		sockaddr_in server;
-		std::unordered_map<int,Player> players;
-	public:
-		void init(){
-			green = BC(Sprite,Surface("gfx/green.png"));
-			red = BC(Sprite,Surface("gfx/red.png"));
-			hp_fill = BC(Surface,"gfx/hp_fill.png");
-			map = BC(TMX,"maps/Map1.tmx",4);
-			server =  Address(GEK::server,7331);
-			Player mc;
-			socket.write(static_cast<Private*>(&mc),sizeof(Private),&server);
-			while(socket.read(static_cast<Private*>(&mc),sizeof(Private))!=sizeof(Private));
-			id = mc.id;
-			players[id] = mc;
-			on[eK_KEYDOWN] = [&](const eK_Event & e){
-				switch(e.key.keysym.sym){
-					case '\033':
-						std::exit(0);
-						break;
-					default:break;
-				};
-			};
-		}
-		void loop(const double &deltatime){
-			Player tmp;
-			while(socket.read(static_cast<Status*>(&tmp),sizeof(Private))!=sizeof(Private));
-			static_cast<Status&>(players[tmp.id]) = tmp;
-			while(socket.read(static_cast<Status*>(&tmp),sizeof(Private))==sizeof(Private))
-				static_cast<Status&>(players[tmp.id]) = tmp;
-			map->camera(0, players[id].position+Vector2(0.5), GEK::screen/map->tile);
-			for(auto& kv:players){
-				kv.second.draw((kv.second.team=='G')?green:red,map);
-				UI(*hp_fill,UI::root(),map->tracecast(kv.second.position-Vector2(0.5)),{0,0.5},{static_cast<double>(kv.second.hp)/(1000),0.02*aspectratio}).draw();
-			}
-			map->camera(1, players[id].position+Vector2(0.5), GEK::screen/map->tile);
-			switch(GEK::mouse.status){
-				case (Mouse::L):
-					players[id].target = map->raycast(GEK::mouse.position);
+struct sGame : public Scene {
+	TMX* map;
+	int id;
+	Sprite* green;
+	Sprite* red;
+	Surface* hp_fill;
+	UDP socket;
+	sockaddr_in server;
+	std::unordered_map<int,Player> players;
+	sGame(){
+		green = BC(Sprite,Surface("gfx/green.png"));
+		red = BC(Sprite,Surface("gfx/red.png"));
+		hp_fill = BC(Surface,"gfx/hp_fill.png");
+		map = BC(TMX,"maps/Map1.tmx",4);
+		server =  Address(GEK->server,7331);
+		Player mc;
+		socket.write(static_cast<Private*>(&mc),sizeof(Private),&server);
+		while(socket.read(static_cast<Private*>(&mc),sizeof(Private))!=sizeof(Private));
+		id = mc.id;
+		players[id] = mc;
+		on[eK_KEYDOWN] = [&](const eK_Event & e){
+			switch(e.key.keysym.sym){
+				case '\033':
+					std::exit(0);
 					break;
-				case (Mouse::R):
-					if(GEK::mouse.updated & Mouse::R)
-						++(players[id].shoot);
 				default:break;
-			}
-			socket.write(static_cast<Private*>(&players[id]),sizeof(Private),&server);
+			};
 		};
+	}
+	Scene* operator()(){
+		Player tmp;
+		while(socket.read(static_cast<Status*>(&tmp),sizeof(Private))!=sizeof(Private));
+		static_cast<Status&>(players[tmp.id]) = tmp;
+		while(socket.read(static_cast<Status*>(&tmp),sizeof(Private))==sizeof(Private))
+			static_cast<Status&>(players[tmp.id]) = tmp;
+		map->camera(0, players[id].position+Vector2(0.5), GEK->screen/map->tile);
+		map->camera(1, players[id].position+Vector2(0.5), GEK->screen/map->tile);
+		for(auto& kv:players){
+			kv.second.draw((kv.second.team=='G')?green:red,map);
+			UI(*hp_fill,UI::root(),map->tracecast(kv.second.position-Vector2(0.5)),{0,0.5},{static_cast<double>(kv.second.hp)/(1000),0.02*aspectratio}).draw();
+		}
+		switch(GEK->mouse.status){
+			case (Mouse::L):
+				players[id].target = map->raycast(GEK->mouse.position);
+				break;
+			case (Mouse::R):
+				if(GEK->mouse.updated & Mouse::R)
+					++(players[id].shoot);
+			default:break;
+		}
+		socket.write(static_cast<Private*>(&players[id]),sizeof(Private),&server);
+		tmp = players[id];
+		players.clear();
+		players[id] = tmp;
+		return this;
+	};
 };
